4ede9e43b50fd8457320bc51a61c67d8bc954fa9
luis arthur <luisarthurlards03@gmail.com>
2022-04-19 17:57:23 -0300

    fznd m

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const marcas= document.querySelector(".marcas");
-const tit=document.querySelector(".parte_da_direita");
+document.addEventListener("mousemove",f);
+const corpo = document.querySelector("body"); 
+var f=(e)=>{
+        /*const corpo=document.querySelector("#body"); */
+        var x = e.clientX;
+        var y = e.clientY;
+        console.log("-------------------")
+        console.log(x);
+        console.log(y);
+        console.log("__")
+        fundo = ("linear-gradient(180deg, rgba(255,255,255,0.49343487394957986) 77%, rgba(0,255,252,0.8715861344537815) 100%);");
+        console.log(fundo)
+        console.log(corpo.style['background']);
+        corpo.style["background"] = fundo;
+        console.log("___")
+        console.log(fundo)
+        console.log(corpo.style.background);
+        console.log("-------------------")
+        }
