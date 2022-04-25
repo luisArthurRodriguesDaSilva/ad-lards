dd891cddd7ee03799a6fea5a64079e10e3fe3c53
luis arthur <luisarthurlards03@gmail.com>
2022-04-24 23:54:49 -0300

    melhoria na transformação

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ bode.addEventListener('mousemove', e => {
         var x = e.clientX;
         var y = e.clientY;
         console.log(x + " e " + y );
-        gradient="linear-gradient(" + ((Math.atan(x-(screen.width/2),y-(screen.height/2)))*360)/3.14+ "deg, rgba(255,255,255,0.19343487394957986) 25%, rgba(0,255,252,0.8715861344537815) 100%)";
+        gradient="linear-gradient(" + ((((Math.atan2(y-(screen.height/2),x-(screen.width/2))))*360/6.28)+270)+ "deg, rgba(255,255,255,0.19343487394957986) 25%, rgba(0,255,252,0.8715861344537815) 100%)";
         bode.style.backgroundImage= gradient;
         bode.style.MozBackground = gradient;
         bode.style.WebkitBackground = gradient;
