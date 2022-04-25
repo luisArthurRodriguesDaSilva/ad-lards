3b3f969e6e8d0f8b0ae1f9a4c1c060e250a3b663
luis arthur <luisarthurlards03@gmail.com>
2022-04-25 00:28:40 -0300

    intensidade proporcional a distancia + correções de linguagem

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ bode.addEventListener('mousemove', e => {
         var x = e.clientX;
         var y = e.clientY;
         console.log(x + " e " + y );
-        gradient="linear-gradient(" + ((((Math.atan2(y-(screen.height/2),x-(screen.width/2))))*360/6.28)+270)+ "deg, rgba(255,255,255,0.19343487394957986) 25%, rgba(0,255,252,0.8715861344537815) 100%)";
+        var distancia =  Math.sqrt(Math.pow(y-(screen.height/2),2) + Math.pow(x-(screen.width/2),2))
+        const distancia_maxima = Math.sqrt(Math.pow(screen.height,2)+Math.pow(screen.width,2))
+        gradient="linear-gradient(" + ((((Math.atan2(y-(screen.height/2),x-(screen.width/2))))*360/6.28)+270)+ "deg, rgba(255,255,255,0.10343487394957986)" + (distancia_maxima/distancia+10) + "%, rgba(0,255,252,0.7715861344537815) 100%)";
         bode.style.backgroundImage= gradient;
         bode.style.MozBackground = gradient;
         bode.style.WebkitBackground = gradient;
