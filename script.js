2aa5e9ffc35d2f1e3eae56c3e0c90ddfe1917da4
luis arthur <luisarthurlards03@gmail.com>
2022-04-25 00:55:14 -0300

    cor pra dividir

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ bode.addEventListener('mousemove', e => {
         console.log(x + " e " + y );
         var distancia =  Math.sqrt(Math.pow(y-(screen.height/2),2) + Math.pow(x-(screen.width/2),2))
         const distancia_maxima = Math.sqrt(Math.pow(screen.height,2)+Math.pow(screen.width,2))
-        gradient="linear-gradient(" + ((((Math.atan2(y-(screen.height/2),x-(screen.width/2))))*360/6.28)+270)+ "deg, rgba(255,255,255,0.10343487394957986)" + (distancia_maxima/distancia+10) + "%, rgba(0,255,252,0.7715861344537815) 100%)";
+        gradient="linear-gradient(" + ((((Math.atan2(y-(screen.height/2),x-(screen.width/2))))*360/6.28)+270)+ "deg, rgba(255,255,255,0.1) 0%,rgba(0,206,255,0.2) 50% ,rgba(0,255,252,0.7715861344537815) 100%)";
         bode.style.backgroundImage= gradient;
         bode.style.MozBackground = gradient;
         bode.style.WebkitBackground = gradient;
