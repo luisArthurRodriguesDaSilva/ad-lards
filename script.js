c0e4682e4cfe7abf809ab6c63934289fadeb8669
luis arthur <luisarthurlards03@gmail.com>
2022-06-13 21:47:18 -0300

    responsivo

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const ProductsQuantity = 7
+const ProductsQuantity = 7;
+const pathImgs = ['imagens/LUAR com insta e whatsapp.png', 'imagens/LUAR só o nome.png','imagens/LUAR perfume.png'];
+const main = document.querySelector('main');
+
+const createProduct = () => {
+  for(let i = 0; i < 50; i+=1){
+    main.innerHTML += 
+    `
+    <section>
+    <div class="cima">
+        <img src="${pathImgs[i%3]}" alt="">
+    </div>
+    <div class="baixo">
+        <button class="btn btn-sm">
+            comprar
+        </button>
+    </div>
+</section>
+`;
+  }
+}
+
+createProduct();
