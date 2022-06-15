152f0dab80b7a65d3283b7ab4a8ae975c22343d6
luis arthur <luisarthurlards03@gmail.com>
2022-06-14 21:55:22 -0300

    refact css

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const ProductsQuantity = 7;
-const pathImgs = ['imagens/LUAR com insta e whatsapp.png', 'imagens/LUAR só o nome.png','imagens/LUAR perfume.png'];
+const pathImgs = ['imagens/o-boticario-logo.svg','imagens/LUAR com insta e whatsapp.png', 'imagens/LUAR só o nome.png','imagens/LUAR perfume.png'];
 const main = document.querySelector('main');
 
 const createProduct = () => {
   for(let i = 0; i < 50; i+=1){
 
-    let text = `olá, desejo comprar o produto ${i%3 + 1}`
+    let text = `olá, desejo comprar o produto ${i%4 + 1}`
     let wpptext = text.replaceAll(' ','%20')
 
     main.innerHTML += 
     `
     <section>
     <div class="cima">
-        <img src="${pathImgs[i%3]}" alt="">
+        <img src="${pathImgs[i%4]}" alt="">
     </div>
     <div class="baixo">
         <a href="https://wa.me/5522998947260?text=${wpptext}" target="_blank" class="buy-btn btn btn-sm" id="produto-${i}">
-            comprar
+           comprar <img id="wpp-logo" src="imagens/b24b5255a15e38ebe07b12094abdca65.png">
         </a>
     </div>
 </section>
