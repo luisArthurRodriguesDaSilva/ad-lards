868ccfebd6720e3e1b9e2065d39253dae9e4d4dd
luis arthur <luisarthurlards03@gmail.com>
2022-06-15 15:09:56 -0300

    tabelinha

---
+++ relat.js
@@ -0,0 +1,22 @@
+const tabelinha = {
+  'adressesOfImg' :  [
+    
+  'imagens/o-boticario-logo.svg',
+  'imagens/LUAR com insta e whatsapp.png',
+  'imagens/LUAR só o nome.png',
+  'imagens/LUAR perfume.png'
+]
+
+  ,
+
+  'names' : [
+
+    'nome0',
+    'nome1',
+    'nome2',
+    'nome3',
+
+  ]
+};
+
+export default tabelinha;
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
+import tabelinha from './relat.js';
 const ProductsQuantity = 7;
-const pathImgs = ['imagens/o-boticario-logo.svg','imagens/LUAR com insta e whatsapp.png', 'imagens/LUAR só o nome.png','imagens/LUAR perfume.png'];
 const main = document.querySelector('main');
 
 const createProduct = () => {
-  for(let i = 0; i < 50; i+=1){
+  for(let i = 0; i < tabelinha.adressesOfImg.length; i+=1){
 
-    let text = `olá, desejo comprar o produto ${i%4 + 1}`
+    let text = `olá, desejo comprar o produto ${tabelinha.names[i]}`;
     let wpptext = text.replaceAll(' ','%20')
 
     main.innerHTML += 
     `
     <section>
     <div class="cima">
-        <img src="${pathImgs[i%4]}" alt="">
+        <img src="${tabelinha.adressesOfImg[i]}" alt="imagem do produto ${tabelinha.names[i]}">
     </div>
     <div class="baixo">
         <a href="https://wa.me/5522998947260?text=${wpptext}" target="_blank" class="buy-btn btn btn-sm" id="produto-${i}">
@@ -23,7 +23,6 @@ const createProduct = () => {
 `;
   }
 }
-
 createProduct();
 
-document.querySelectorAll('.buy-btn')
+document.querySelectorAll('.buy-btn');
