fee19a97dc49ad35c82a986d03e4b4341a644013
luis arthur <luisarthurlards03@gmail.com>
2022-06-26 20:09:40 -0300

    the js with change price

--- the.py
+++ the.py
@@ -1,8 +1,14 @@
-
 import os
 adresses = list(map(lambda x: (str('./imagens/' + x)) ,os.listdir('./imagens')));
 
 names=list(map(lambda x: (str(x.split('.')[0].replace('-',' '))) ,os.listdir('./imagens')));
+
+prices = []
+
+for i in range(len(adresses)):
+    print(f"preço do {names[i]} é")
+    prices.append(input()66)
+    print(prices)
 
 print(os.listdir('imagens'))
 with open("relat.js", 'w') as file:
@@ -12,6 +18,8 @@ with open("relat.js", 'w') as file:
   'adressesOfImg' :[""" + "'" + str("',\n'".join(adresses)) + "'" + """]
   ,
   'names' : [""" + "'" + str("',\n'".join(names)) + "'" + """]
+  ,
+  'prices' : [""" + "'" + str("',\n'".join(prices)) + "'" + """]
 };
 
 export default tabelinha;
