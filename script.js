1be27f52ca9b1ce2af50ce5f6a53bd4287f248fc
luis arthur <luisarthurlards03@gmail.com>
2022-06-27 00:21:28 -0300

    /2

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ const rmProduct = (ID,produto,listCard,dive,span) => {
 }
 
 const changeMessageOfCardBuy = () => {
-  const produtosComprados = document.querySelectorAll('#carrinho-list .prod');
+  const produtosComprados = document.querySelector('#carrinho-list').childNodes;
   let text = 'Quero comprar%0A%0A';
   for(let i of produtosComprados){
     text += `${i.innerText}%0A`;
